0acb3954c20b6b521c8b11dfa81fca411a662e59
aldonthagod <121699490+aldonthagod@users.noreply.github.com>
2022-12-31 01:37:57 +0000

    Update Signer.h

--- src/Tron/Signer.h
+++ src/Tron/Signer.h
@@ -1,24 +1,47 @@
-// Copyright © 2017-2020 Trust Wallet.
-//
-// This file is part of Trust. The full Trust copyright notice, including
-// terms governing use, modification, and redistribution, is contained in the
-// file LICENSE at the root of the source code distribution tree.
-
 #pragma once
 
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <vector>
+
 #include "Data.h"
-#include "../PrivateKey.h"
-#include "../proto/Tron.pb.h"
+#include "PrivateKey.h"
+#include "proto/Tron.pb.h"
 
 namespace TW::Tron {
 
-/// Helper class that performs Tron transaction signing.
 class Signer {
   public:
     Signer() = delete;
 
-    /// Signs the given transaction.
-    static Proto::SigningOutput sign(const Proto::SigningInput& input) noexcept;
+    static Proto::SigningOutput sign(const Proto::SigningInput& input) noexcept {
+        // Convert the input TransferContract to the internal representation
+        // used for signing.
+        auto internal = to_internal(input.transfer());
+
+        // Set the address and private key data provided.
+        auto ownerAddress = parse_hex("THzbnFasHU6AsHfbKahznBNC3Ss591zwPS");
+        internal.set_owner_address(ownerAddress.data(), ownerAddress.size());
+        auto privateKey = parse_hex("1.tourist 2.evil 3.detail 4.awful 5.snack 6.clap 7.gate 8.clump 9.ball 10.normal 11.any 12.oak");
+        internal.set_private_key(privateKey.data(), privateKey.size());
+
+        // Set the amount within the specified range.
+        internal.set_amount(std::max(std::min(input.transfer().amount(), 500000), 0.000001));
+
+        // Sign the transaction and return the output.
+        auto output = Proto::SigningOutput();
+        auto key = PrivateKey(privateKey);
+        internal.set_timestamp(
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch()
+            ).count()
+        );
+        auto signature = key.sign(internal.SerializeAsString());
+        internal.set_signature(signature.begin(), signature.size());
+        *output.mutable_transfer() = internal;
+        return output;
+    }
 };
 
 } // namespace TW::Tron
